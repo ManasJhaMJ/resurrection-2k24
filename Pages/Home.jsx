cdd24c34d7766c5f96e5ef259a7da57777b6ad99
manasjhamj <jhamanas939@gmail.com>
2024-08-22 20:57:54 +0530

    initial Commit

---
+++ Pages/Home.jsx
@@ -0,0 +1,25 @@
+import Navbar from "../src/components/Navbar/Navbar"
+import Landing from "../src/components/Landing/Landing"
+import About from "../src/components/About/About"
+import Events from "../src/components/Events/Events"
+import Theme from "../src/components/Theme/Theme"
+import Video from "../src/components/Video/Video"
+import Gallery from "../src/components/Gallery/Gallery"
+import Footer from "../src/components/Footer/Footer"
+
+function Home() {
+    return (
+        <>
+            <Navbar />
+            <Landing />
+            <About />
+            <Events />
+            <Theme />
+            <Video />
+            <Gallery />
+            <Footer />
+        </>
+    )
+}
+
+export default Home
