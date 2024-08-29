77dd384087fbba45f6a7429cc0b7f1865b4c4bff
HaniRawat <120402194+HaniRawat@users.noreply.github.com>
2024-08-29 21:23:55 +0530

    changed events

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";  // Import slick-carousel styles
+import "slick-carousel/slick/slick.css";  
 import "slick-carousel/slick/slick-theme.css";  // Import slick-carousel theme
 
 const Events = () => {
